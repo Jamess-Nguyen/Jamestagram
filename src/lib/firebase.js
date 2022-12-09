973cc0b81b8c13946dbb6d17f7a2b83e936ed5c9
jamess-nguyen <haimenguyen@gmail.com>
2022-12-08 21:53:38 -0800

    seeded db

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+ apiKey: "AIzaSyBWZFFfAQ5unxI4S0P9lBdZniyF9Vulx9w",
+  authDomain: "jamestagram-33038.firebaseapp.com",
+  projectId: "jamestagram-33038",
+  storageBucket: "jamestagram-33038.appspot.com",
+  messagingSenderId: "163004871266",
+  appId: "1:163004871266:web:67a6ccb13177e6885ad73b"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+
+export { firebase, FieldValue };
