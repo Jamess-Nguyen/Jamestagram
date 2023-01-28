46cdb71af6741799e2c79b766082881ff37527f3
jamess-nguyen <haimenguyen@gmail.com>
2023-01-27 20:47:17 -0800

    loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,16 +1,7 @@
-// import Loader from 'react-loader-spinner';
-
 export default function ReactLoader() {
   return (
     <div>
-      hi
+      Loading
     </div>
-    // <Loader
-    //   type="TailSpin"
-    //   color="#00000059"
-    //   height={70}
-    //   width={70}
-    //   className="flex justify-center mt-12"
-    // />
   );
 }
