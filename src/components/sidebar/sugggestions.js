5edbfe9b72fe7d4eb35df53043d31053e6761133
jamess-nguyen <haimenguyen@gmail.com>
2023-01-27 16:18:30 -0800

    refactored comments

--- src/components/sidebar/sugggestions.js
+++ src/components/sidebar/sugggestions.js
@@ -17,11 +17,6 @@ export default function Suggestions({ userId, following, loggedInUserDocId }) {
       suggestedProfiles();
     }
   }, [userId]);
-  // hint: use the firebase service (call using userId)
-  // getSuggestedProfiles
-  // call the async function ^^^^ within useEffect
-  // store it in state
-  // go ahead and render (wait on the profiles as in 'skeleton')
 
   return !profiles ? (
     <Skeleton count={1} height={150} className="mt-5" />
