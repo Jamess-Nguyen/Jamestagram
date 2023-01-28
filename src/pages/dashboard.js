36e9cf687fe48e95f0875e18b11b041a7eb61960
jamess-nguyen <haimenguyen@gmail.com>
2023-01-27 16:16:43 -0800

    api components built

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import Header from '../components/header';
-import Sidebar from '../components/sidebar/sidebar';
+import Sidebar from '../components/sidebar/index';
 import Timeline from '../components/timeline';
 
 export default function Dashboard(){
