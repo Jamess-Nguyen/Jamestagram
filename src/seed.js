b2ce715110a88cacd2503f7c9409ffa0f873a1a4
jamess-nguyen <haimenguyen@gmail.com>
2023-01-28 09:39:54 -0800

    seeding

--- src/seed.js
+++ src/seed.js
@@ -1,43 +1,43 @@
 export function seedDatabase(firebase) {
   const users = [
+    // {
+    //   userId: 'TXEpOpHQy2bt0sHezvGsVe0wWXv2',
+    //   username: 'James',
+    //   fullName: 'James Nguyen',
+    //   emailAddress: 'jamesnguyencareer@gmail.com',
+    //   following: ['2'],
+    //   followers: ['2', '3', '4'],
+    //   dateCreated: Date.now()
+    // },
+    // {
+    //   userId: '2',
+    //   username: 'raphael',
+    //   fullName: 'Raffaello Sanzio da Urbino',
+    //   emailAddress: 'raphael@sanzio.com',
+    //   following: [],
+    //   followers: ['TXEpOpHQy2bt0sHezvGsVe0wWXv2'],
+    //   dateCreated: Date.now()
+    // },
+    // {
+    //   userId: '3',
+    //   username: 'dali',
+    //   fullName: 'Salvador Dalí',
+    //   emailAddress: 'salvador@dali.com',
+    //   following: [],
+    //   followers: ['TXEpOpHQy2bt0sHezvGsVe0wWXv2'],
+    //   dateCreated: Date.now()
+    // },
+    // {
+    //   userId: '4',
+    //   username: 'orwell',
+    //   fullName: 'George Orwell',
+    //   emailAddress: 'george@orwell.com',
+    //   following: [],
+    //   followers: ['TXEpOpHQy2bt0sHezvGsVe0wWXv2'],
+    //   dateCreated: Date.now()
+    // },
     {
-      userId: 'TXEpOpHQy2bt0sHezvGsVe0wWXv2',
-      username: 'James',
-      fullName: 'James Nguyen',
-      emailAddress: 'jamesnguyencareer@gmail.com',
-      following: ['2'],
-      followers: ['2', '3', '4'],
-      dateCreated: Date.now()
-    },
-    {
-      userId: '2',
-      username: 'raphael',
-      fullName: 'Raffaello Sanzio da Urbino',
-      emailAddress: 'raphael@sanzio.com',
-      following: [],
-      followers: ['TXEpOpHQy2bt0sHezvGsVe0wWXv2'],
-      dateCreated: Date.now()
-    },
-    {
-      userId: '3',
-      username: 'dali',
-      fullName: 'Salvador Dalí',
-      emailAddress: 'salvador@dali.com',
-      following: [],
-      followers: ['TXEpOpHQy2bt0sHezvGsVe0wWXv2'],
-      dateCreated: Date.now()
-    },
-    {
-      userId: '4',
-      username: 'orwell',
-      fullName: 'George Orwell',
-      emailAddress: 'george@orwell.com',
-      following: [],
-      followers: ['TXEpOpHQy2bt0sHezvGsVe0wWXv2'],
-      dateCreated: Date.now()
-    },
-    {
-      userId: '4',
+      userId: '5',
       username: 'Tech Stack',
       fullName: 'Tech Stack',
       emailAddress: 'TechStack.com',
@@ -53,31 +53,31 @@ export function seedDatabase(firebase) {
   }
 
   // eslint-disable-next-line prefer-const
-  for (let i = 1; i <= 5; ++i) {
-    firebase
-      .firestore()
-      .collection('photos')
-      .add({
-        photoId: i,
-        userId: '2',
-        imageSrc: `/images/users/raphael/${i}.jpg`,
-        caption: 'Saint George and the Dragon',
-        likes: [],
-        comments: [
-          {
-            displayName: 'dali',
-            comment: 'Love this place, looks like my animal farm!'
-          },
-          {
-            displayName: 'orwell',
-            comment: 'Would you mind if I used this picture?'
-          }
-        ],
-        userLatitude: '40.7128°',
-        userLongitude: '74.0060°',
-        dateCreated: Date.now()
-      });
-  }
+  // for (let i = 1; i <= 5; ++i) {
+  //   firebase
+  //     .firestore()
+  //     .collection('photos')
+  //     .add({
+  //       photoId: i,
+  //       userId: '2',
+  //       imageSrc: `/images/users/raphael/${i}.jpg`,
+  //       caption: 'Saint George and the Dragon',
+  //       likes: [],
+  //       comments: [
+  //         {
+  //           displayName: 'dali',
+  //           comment: 'Love this place, looks like my animal farm!'
+  //         },
+  //         {
+  //           displayName: 'orwell',
+  //           comment: 'Would you mind if I used this picture?'
+  //         }
+  //       ],
+  //       userLatitude: '40.7128°',
+  //       userLongitude: '74.0060°',
+  //       dateCreated: Date.now()
+  //     });
+  // }
 
   for (let i = 1; i <= 5; ++i) {
     firebase
